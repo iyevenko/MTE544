0867809256d31c78991d5a714936a6a48bebe058
Ivan Yevenko <39413391+iyevenko@users.noreply.github.com>
2024-10-09 09:55:07 -0700

    comment laserPlotter

--- laserPlotter.py
+++ laserPlotter.py
@@ -2,19 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utilities import FileReader
 
+"""
+This file creates a scatter plot for the lidar data at a point in time.
+Mostly copied from existing filePlotter.py
+"""
+
 def plot_ranges(filename):
     headers, values=FileReader(filename).read_file()
     ranges = np.array([v[0] for v in values])
     angle_increment = [v[1] for v in values]
+    # Integrate angle_increment
     theta = np.cumsum(angle_increment)
 
+    # Filter out infinite depths
     nonInf = ~np.isinf(ranges)
     ranges = ranges[nonInf]
     theta = theta[nonInf]
 
+    # Convert polar to euclidean
     x = ranges * np.cos(theta)
     y = ranges * np.sin(theta)
 
+    # Scatter plot with proportional x and y axes
     plt.scatter(x,y, s=2)
     plt.axis('equal')
     plt.grid()
